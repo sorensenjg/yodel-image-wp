2b8222f69369daec4bd105d84487926aff3042b5
Justin Sorensen <hey@sorensenjg.com>
2025-01-07 13:34:12 -0800

    b

--- src/components/credit-menu.tsx
+++ src/components/credit-menu.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const { config } = window.yodelImageAdmin;
 
 interface CreditMenuProps {
-  className: string;
+  className?: string;
 }
 
 export function CreditMenu({ className }: CreditMenuProps) {
